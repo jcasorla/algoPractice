19d35f7275cd1f2fd7c532403b39f6a272fcdb06
Jose <precado999@gmail.com>
2020-12-14 14:21:41 -0800

    multiStringSearch algoexpert

---
+++ algoexpert/multiStringSearch.js
@@ -0,0 +1,13 @@
+//https://www.algoexpert.io/questions/Multi%20String%20Search
+
+function multiStringSearch(bigString, smallStrings) {
+  const res = [];
+  for (const word of smallStrings) {
+    if (bigString.includes(word)) {
+      res.push(true);
+    } else {
+      res.push(false);
+    }
+  }
+  return res;
+}
