778239830412162366e830f901f412453db46061
Jose <precado999@gmail.com>
2020-12-17 11:14:08 -0800

    shorttestSubArraywithLargestSum

--- superprofMock/shorttestSubArraywithLargestSum.js
+++ superprofMock/shorttestSubArraywithLargestSum.js
@@ -21,13 +21,53 @@
 //cadance algo
 
 function shorttestSubArraywithLargestSum(arr, target) {
-  let p1 = 0;
-  let p2 = 0;
-  let len = arr.length;
-  let globalSum = 0;
-  let sum = 0;
-  let res = { 0: 0 };
-  while (p1 < len) {
-    p2;
+  const len = arr.length;
+  const res = [];
+  //O(N)2
+  for (let i = 0; i < len; i++) {
+    let sum = arr[i];
+    if (sum > target) {
+      res.push([sum, 1]);
+    }
+    for (let j = i + 1; j < len; j++) {
+      const sublen = j - i + 1;
+      sum += arr[j];
+
+      if (sum > target) {
+        res.push([sum, sublen]);
+      }
+    }
   }
+  let max = -Infinity;
+  for (let i = 0; i < res.length; i++) {
+    if (res[i][0] > max) max = res[i][0];
+  }
+
+  return res;
 }
+
+//https://www.geeksforgeeks.org/minimum-length-subarray-sum-greater-given-value/
+function shorttestSubArraywithLargestSum(arr, target) {
+  const len = arr.length;
+  let minlen = arr.length + 1;
+  //O(N)2 | O(1)
+  for (let i = 0; i < len; i++) {
+    let sum = arr[i];
+    if (sum > target) {
+      return 1;
+    }
+    for (let j = i + 1; j < len; j++) {
+      sum += arr[j];
+
+      if (sum > target && j - i + 1 < minlen) {
+        minlen = j - i + 1;
+      }
+    }
+  }
+
+  return minlen;
+}
+
+console.log(shorttestSubArraywithLargestSum([1, 3, 9, 5, 1], 6));
+console.log(shorttestSubArraywithLargestSum([1, 3, 9, 5, 1], 9));
+console.log(shorttestSubArraywithLargestSum([1, 1, 2, 3, 1], 9));
