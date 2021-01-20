fb937bdc1e7a4cc92afdf89aebde42e530ba797f
Jose <precado999@gmail.com>
2021-01-20 10:27:56 -0800

    leetcode runningSum

---
+++ mysql/dailyLeedAndParners.js
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/daily-leads-and-partners/submissions/
+
+select date_id,
+make_name,
+COUNT(DISTINCT(lead_id)) AS unique_leads,
+COUNT(DISTINCT(partner_id)) AS unique_partners
+from DailySales
+group by date_id, make_name
