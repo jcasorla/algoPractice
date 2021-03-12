8764b05abded5dc34893e365141de2d102ab8217
Jose <precado999@gmail.com>
2021-03-11 17:43:15 -0800

    algoexpert - sortedSquaredArray

--- leetCode/sortedSquaredArray.js
+++ leetCode/sortedSquaredArray.js
@@ -15,3 +15,25 @@ function sortedSquaredArray(array) {
   }
   return newArr.sort((a, b) => a - b);
 }
+
+//0(n) |0(n)
+function sortedSquaredArray(array) {
+  const len = array.length;
+  const sortedSquares = new Array(len).fill(0);
+  let smallerValueIdx = 0;
+  let largestValueIdx = array.length - 1;
+
+  for (let i = array.length - 1; i >= 0; i--) {
+    const smallerValue = array[smallerValueIdx];
+    const largerValue = array[largestValueIdx];
+
+    if (Math.abs(smallerValue) > Math.abs(largerValue)) {
+      sortedSquares[i] = smallerValue * smallerValue;
+      smallerValueIdx++;
+    } else {
+      sortedSquares[i] = largerValue * largerValue;
+      largestValueIdx--;
+    }
+  }
+  return sortedSquares;
+}
