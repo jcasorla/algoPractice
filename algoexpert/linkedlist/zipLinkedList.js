e11e3182ed2c4cc552adb673158b6b9110f24044
Jose <precado999@gmail.com>
2021-03-14 18:41:54 -0700

    algoexpert - zipLinkedList

--- algoexpert/linkedlist/zipLinkedList.js
+++ algoexpert/linkedlist/zipLinkedList.js
@@ -8,6 +8,7 @@ class LinkedList {
   }
 }
 
+//my solution
 //0(n^2) | 0(1)
 function zipLinkedList(linkedList) {
   let r1 = linkedList;
@@ -42,4 +43,57 @@ function zipLinkedList(linkedList) {
     r2 = r2.next;
   }
   return linkedList;
+}
+
+//algoexpert solution
+//0(n) | 0(1)
+function zipLinkedList(linkedList) {
+  if (linkedList.next === null || linkedList.next.next === null)
+    return linkedList;
+
+  const firstHalfHead = linkedList;
+  const secondHalfHead = splitLinkedList(linkedList);
+  const reversedSecondHalfHead = reverseLinkedList(secondHalfHead);
+  return interweaveLinkedList(firstHalfHead, reversedSecondHalfHead);
+}
+
+function splitLinkedList(linkedList) {
+  let slow = linkedList;
+  let fast = linkedList;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  const secondHalfHead = slow.next;
+  slow.next = null;
+  return secondHalfHead;
+}
+
+function reverseLinkedList(head) {
+  let prev = null;
+  let cur = head;
+  while (cur) {
+    const nextNode = cur.next;
+    cur.next = prev;
+    prev = cur;
+    cur = nextNode;
+  }
+  return prev;
+}
+function interweaveLinkedList(linkedList1, linkedList2) {
+  let r1 = linkedList1;
+  let r2 = linkedList2;
+
+  while (r1 && r2) {
+    const r1Next = r1.next;
+    const r2Next = r2.next;
+
+    r1.next = r2;
+    r2.next = r1Next;
+
+    r1 = r1Next;
+    r2 = r2Next;
+  }
+  return linkedList1;
 }
